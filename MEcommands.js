ee0e5cd04d45e75e042d42471bff898c7c7e06f5
hrij786 <mohamed.ha@zohocorp.com>
2025-05-20 19:32:43 +0530

    Update MEcommands.js

--- MEcommands.js
+++ MEcommands.js
@@ -285,6 +285,10 @@ function main(a) {
     a.completed({ allowEvent: !0 });
 }
 function mainHandleAttachments(a) {
+  console.log("hello mainHandleAttachments");
+    a.completed({ allowEvent: !0 });
+}
+function onMessageSendHandler(a) {
   console.log("hello main");
     a.completed({ allowEvent: !0 });
 }
