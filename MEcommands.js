346c349158f6e2d0a1a770d0c26c199a1b728dee
hrij786 <mohamed.ha@zohocorp.com>
2025-05-20 18:41:46 +0530

    Update MEcommands.js

--- MEcommands.js
+++ MEcommands.js
@@ -2,7 +2,10 @@ function main(a) {
   console.error("Inside main");
   event.completed({ allowEvent: true });
 }
-
+function hellow(){
+  console.log("hellow world");
+}
+  
 async function mainHandleAttachments(a) {
   console.error("Inside mainHandleAttachments");
 }
