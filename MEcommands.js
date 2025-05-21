b83d971410b2fd9aa1488a08d0f5446197251fc3
hrij786 <mohamed.ha@zohocorp.com>
2025-05-21 11:35:24 +0530

    Update MEcommands.js

--- MEcommands.js
+++ MEcommands.js
@@ -1,22 +1,3 @@
-const SUPPORTED_PLATFORMS = ["MacIntel", "Win32"],
-  AVAILABLE_SERVICE_PORTS = [4631, 4641, 4651],
-  REQUEST_TIMEOUT = 3e5,
-  CONTENT_HEADERS = { "Content-Type": "application/json;charset=utf-8" },
-  POST_HEADERS = {
-    "Content-Type": "application/json;charset=utf-8",
-    "Access-Control-Request-Method": "POST"
-  },
-  OFFICE_HOST_NAMES = {
-    OUTLOOK_CLIENT: "Outlook",
-    OUTLOOK_WEB_ACCESS: "OutlookWebApp"
-  };
-
-let officeHostName,
-  mailboxItem,
-  agentWebSrvcPort,
-  addinBrowserStorage = window.localStorage,
-  serviceUrl = "http://127.0.0.1";
-
 Office.initialize = function (a) {
   mailboxItem = Office.context.mailbox.item;
   officeHostName = Office.context.mailbox.diagnostics.hostName;
