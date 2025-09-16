a54a8812a6f40dc5a6ea87f97ddab134606b331b
hrij786 <mohamed.ha@zohocorp.com>
2025-09-16 13:13:51 +0530

    Update MEcommands.js

--- MEcommands.js
+++ MEcommands.js
@@ -128,7 +128,7 @@ function onMessageSendHandler(event) {
   console.log("OnSend triggered.");
   try {
 	  // Add-in runs only on Windows with Outlook Mailbox API v1.8+
-	  if("Win32" === navigator.platform && Office.context.requirements.isSetSupported("Mailbox", 1.8) && g_OfficeHostName == "Outlook") {
+	  if("Win32" === navigator.platform && Office.context.requirements.isSetSupported("Mailbox", 1.8) && g_OfficeHostName === "Outlook") {
     	eventValidator(event);
 	  }
 	  else {
